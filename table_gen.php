3871b3b434ea12c27967f73a0e64270004768b21
william Huang <D1114182002@gm.lhu.edu.tw>
2024-10-04 15:47:13 +0800

    表格

--- table_gen.php
+++ table_gen.php
@@ -2,4 +2,15 @@
    $row_num = $_GET['row_num'];
    $col_num = $_GET['col_num'];
    echo '輸入大小:'.$row_num.'列'.$col_num.'行';
+   echo'<table border="1">';
+   for($i = 0; $i < $row_num; $i++) 
+   {
+    echo '<tr>';
+    for($j = 0; $j < $col_num; $j++)
+    {
+        echo '<td>'.($i+1).'列'.($j+1).'行'.'</td>';
+    }
+    echo '<td>';
+
+   }
 ?>
